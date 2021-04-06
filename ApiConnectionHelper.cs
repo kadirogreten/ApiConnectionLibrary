6d2f468f1a48df72b69af8fef22eb2e4db761a7b
KCESoftware <cagri.caglar@kceyazilim.com>
2021-04-06 12:09:12 +0300

    Add project files.

---
+++ ApiConnectionHelper.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiConnectionLibrary
+{
+    public class ApiConnectionHelper<T>
+    {
+        public static async Task<T> GetAsync(string UrlEndPoint)
+        {
+            T result;
+            using (var httpClient = new HttpClient())
+            {
+                using (var response = await httpClient.GetAsync($"{UrlEndPoint}"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    result = JsonConvert.DeserializeObject<T>(apiResponse);
+                }
+            }
+            return result;
+        }
+
+
+        public static async Task<T> PostAsync(ConnectApiModel model)
+        {
+            T result;
+            using (var httpClient = new HttpClient())
+            {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(model.Parameters), Encoding.UTF8, "application/json");
+
+                using (var response = await httpClient.PostAsync(model.UrlEndPoint, content))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    result = JsonConvert.DeserializeObject<T>(apiResponse);
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ ConnectApiModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApiConnectionLibrary
+{
+    public class ConnectApiModel
+    {
+        public string UrlEndPoint { get; set; }
+        public object Parameters { get; set; }
+    }
+}
